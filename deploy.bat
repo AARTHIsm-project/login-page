da5517cf3d85e8d661149c566e030ad8e7378600
Aarthi SM <aarthism.adroittech@gmail.com>
2025-08-04 15:30:57 +0530

    test file

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,19 @@
 @echo off
-echo Starting deployment...
-REM Simulate deploy (example)
-mkdir C:\DeployedLoginPage
-xcopy /Y /E /I "src" "C:\DeployedLoginPage"
-echo  Deployed!
+echo  Starting deployment...
+
+REM Target folder for deployment (you can customize this)
+set TARGET=C:\DeployedLoginPage
+
+REM Create the target folder if it doesn’t exist
+if not exist "%TARGET%" (
+    mkdir "%TARGET%"
+)
+
+REM Copy HTML, CSS, and images folder
+echo  Copying files to %TARGET%...
+xcopy /Y /I "index.html" "%TARGET%"
+xcopy /Y /I "style.css" "%TARGET%"
+xcopy /Y /E /I "images" "%TARGET%\images"
+
+echo  Deployment complete at %TARGET%
+exit /b 0
